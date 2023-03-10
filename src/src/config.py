2f84dce730c473c5fcc66b958676c75ccec6bc13
MorillaGit <cristian_morilla@mi.unc.edu.ar>
2023-03-10 10:19:13 -0300

    REMOVE files

--- src/src/config.py
+++
@@ -1,5 +0,0 @@
-config = {
-    # "CHB_FOLDER_DIR": "/c/Users/crist/Documents/tesis/data/chb-mit-scalp-eeg-database-1.0.0"
-
-    "CHB_FOLDER_DIR": "C:/Users/crist/Documents/tesis/data/chb-mit-scalp-eeg-database-1.0.0"
-}
--- src/src/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='sz_utils',
-    version='0.1',
-    packages=find_packages(),
-)
